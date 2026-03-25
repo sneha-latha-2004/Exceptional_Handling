784fc28ac3a22bcdbb69c35786d9db1f3224b6d8
sneha-latha-2004 <sneha22ec052@drttit.edu.in>
2026-03-25 13:38:33 +0530

    handle

---
+++ handle_spe.py
@@ -0,0 +1,8 @@
+try:
+    a = int(input("Enter number: "))
+    b = int(input("Enter number: "))
+    print(a / b)
+except ZeroDivisionError:
+    print("Cannot divide by zero!")
+except ValueError:
+    print("Invalid input!")
